4592ca8464af60d00f95e905ab2217c9d0834116
Velda <velda07dias@gmail.com>
2024-01-18 09:56:34 +0530

    git assignment commit

---
+++ GitHubProject/GitHub/src/pksybca/assignment.java
@@ -0,0 +1,27 @@
+package pksybca;
+
+import java.util.Scanner;
+
+public class assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("enter the first number:");
+		int num1=scan.nextInt();
+		
+		System.out.println("enter the first number:");
+		int num2=scan.nextInt();
+		
+		//add the numbers
+		int  sum=num1+num2;
+		
+		System.out.println("sum:"+sum);
+		
+		
+
+	}
+
+}
